4c70bc06f2a28c7824038e5c387129ebf4ab7db7
Charlie <caam_1275@hotmail.com>
2018-03-10 13:19:13 -0600

    Actualizacion

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./app/scss/main.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./app/scss/main.scss', ['sass']);
+});
+
+gulp.task('default', [ 'sass','sass:watch' ]);
